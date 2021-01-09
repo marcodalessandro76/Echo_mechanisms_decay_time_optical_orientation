2f47690bcc56e23ae5559ef32382882f1c192da5
Marco D'Alessandro <marco.dalessandro@ism.cnr.it>
2021-01-09 04:43:37 +0100

    update

--- Routines.py
+++ Routines.py
@@ -1,36 +1,57 @@
 from mppi import Utilities as U, Parsers as P
 import numpy as np
 
-def eval_transition_dipole(ndb,kpoint=0,transition=[0,1],component=0):
-    """
-    Compute the transition dipole of a specific transition
-    
-    Args:
-        ndb : name of the dipoles database
-        kpoint : selected kpoint
-        transition : transitions activated by the pump
-        component : cartesian component of the dipole
-    """
-    dipoles = U.get_variable_from_db(ndb,'DIP_iR')
-    dip = dipoles[kpoint][transition[0]][transition[1]][component]
-    #dip_mod = np.sqrt(dip[0]**2+dip[1]**2)
-    return dip
 
-def eval_trans_energy(save,kpoint=0,transition=[0,1],
-                      set_scissor=None,set_gap=None,set_direct_gap=None):
+def eval_pulse_parameters(dipole,field_int,width,verbose=True):
     """
-    Compute the energy of the selected transition
+    Compute the coupling frequency and the pulse area in function of the values of the
+    transition dipole and of the field intensity.
     
     Args:
-        save : SAVE folder that contain the ns.db1 database
-        kpoint : selected kpoint
-        transition : transitions activated by the pump
-        component : cartesian component of the dipole
+        dipole : value of the dipole for the selected kpoint and transition
+        field_int : field intensity in kW/cm^2
+        width : in fs
+    
+    Returns:
+        (coupling_frequency,theta) : a tuple  with the Rabi coupling frequency 
+            (in fs^-1) and the pulse area theta
+        
     """
-    data = P.NsdbsParser(save=save,verbose=False)
-    trans_energy = data.get_transitions(initial=[transition[0]],final=[transition[1]],
-                    set_scissor=set_scissor,set_gap=set_gap,set_direct_gap=set_direct_gap)
-    return trans_energy[kpoint][0]
+    Z0 = U.vacuum_impedence
+    field_int = field_int*1e3*1e4 #W/m^2
+    field_amp = np.sqrt(Z0*field_int) #V/m
+    field_amp = field_amp*U.Bohr_radius #V/a0 in atomic units
+    dip_mod = np.linalg.norm(dipole)
+    coupling_frequency = dip_mod*field_amp*2*np.pi/U.Planck_ev_ps*1e-3 #fs^-1
+    
+    theta = np.sqrt(2*np.pi)*width*coupling_frequency
+    if verbose:
+        print('coupling frequency (THz):',coupling_frequency*1e3)
+        print('pulse area :',theta)
+    return coupling_frequency,theta
+
+def eval_field_intensity(dipole,theta,width,verbose=True):
+    """
+    Compute the field intensity that produce the pulse are given as input.
+        
+    Args:
+        dipole : name of the dipoles database
+        theta : pulse area
+        width : 
+    
+    """
+    Z0 = U.vacuum_impedence
+    dip_mod = np.linalg.norm(dipole)
+    coupling_frequency = theta/(np.sqrt(2*np.pi)*width) #fs^-1
+    field_amp = coupling_frequency/(dip_mod*2*np.pi/(U.Planck_ev_ps*1e3)) #V/a0 in atomic units
+    field_amp = field_amp/U.Bohr_radius #V/m
+    field_int = field_amp**2/Z0 #W/m^2
+    field_int = field_int*1e-3*1e-4 #kW/cm^2
+    if verbose: print('field intensity (kW/cm^2) :',field_int)
+    return field_int
+
+
+####################################################################################
 
 def eval_effective_field_int(width,pump_energy,trans_energy,verbose=False):
     """
@@ -68,66 +89,6 @@ def eval_effective_field_int(width,pump_energy,trans_energy,verbose=False):
         print('scale',scale)
     return scale
 
-def eval_pulse_area_single_trans(dip_mod,trans_energy,field_int,width,pump_energy,verbose=False):
-    """
-    Compute the coupling frequency and the pulse area in function of the field intensity,
-    for a single k-point and a single transition
-    
-    Args:
-        dip_mod : value of the dipole for the selected transition
-        trans_energy : energy of the transition
-        field_int : field intensity in kW/cm^2
-        width : the width of the field in fs
-        kpoint : selected kpoint
-        transition : transitions activated by the pump
-        component : cartesian component of the dipole
-    """
-    Z0 = U.vacuum_impedence
-    eff_field_int = field_int*eval_effective_field_int(width,pump_energy,trans_energy)
-    eff_field_int = eff_field_int*1e3*1e4 #W/m^2
-    field_amp = np.sqrt(Z0*eff_field_int) #V/m
-    field_amp = field_amp*U.Bohr_radius #V/a0 in atomic units
-    coupling_frequency = dip_mod*field_amp*2*np.pi/U.Planck_ev_ps #THz
-    
-    theta = np.sqrt(2*np.pi)*width*coupling_frequency*1e-3
-    if verbose:
-        print('coupling frequency (THz):',coupling_frequency)
-        print('pulse area :',theta)
-    return coupling_frequency,theta
-
-
-
-
-
-
-
-
 ##########################################################################################
 
 
-def eval_field_intensity(ndb,theta,width,kpoints=None,transitions=[[0,1]],component=0):
-    """
-    Compute the field intensity that produce the pulse are given as input.
-    The transition dipoles are computed by averaging the dipole over the kpoints provided
-    in the kpoints field. If the field is None all the kpoints of the ndb database are
-    considered. The function sums over all the transitions included in the
-    transitions field.
-    
-    Args:
-        ndb : name of the dipoles database
-        theta : pulse area
-        width : the width of the field in fs
-        kpoints : list with the selected kpoint. If None all the kpoints of the database are included
-        transitions : list with the transitions activated by the pump
-        component : cartesian component of the dipole
-    """
-    Z0 = U.vacuum_impedence
-    dip_mod = eval_transition_dipole(ndb,kpoints=kpoints,transitions=transitions,component=component)
-    
-    coupling_frequency = theta/(np.sqrt(2*np.pi)*width*1e-3) #THz
-    field_amp = coupling_frequency/(dip_mod*2*np.pi/U.Planck_ev_ps) #V/a0 in atomic units
-    field_amp = field_amp/U.Bohr_radius #V/m
-    field_int = field_amp**2/Z0 #W/m^2
-    field_int = field_int*1e-3*1e-4 #kW/cm^2
-    print('field intensity (kW/cm^2) :',field_int)
-    return field_int
